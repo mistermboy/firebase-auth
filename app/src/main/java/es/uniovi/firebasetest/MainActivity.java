f222f2ec80cc145454275c0d8e1a54db2d2dd9aa
Pablo Menéndez Suárez ◔_◔ <pabloyo97@hotmail.com>
2018-11-30 23:57:27 +0100

    facebook login

--- app/src/main/java/es/uniovi/firebasetest/MainActivity.java
+++ app/src/main/java/es/uniovi/firebasetest/MainActivity.java
@@ -6,16 +6,29 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,11 +36,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText TextEmail;
     private EditText TextPassword;
     private Button btnRegistrar, btnLogin;
+    private LoginButton facebookButtom;
     private ProgressDialog progressDialog;
 
 
     //Declaramos un objeto firebaseAuth
     private FirebaseAuth firebaseAuth;
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +62,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         progressDialog = new ProgressDialog(this);
 
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        facebookButtom = findViewById(R.id.login_button);
+        facebookButtom.setReadPermissions("email","public_profile");
+        facebookButtom.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("CANCEL","CANCEL");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("ERROR",error.toString());
+
+            }
+        });
+
         //asociamos un oyente al evento clic del botón
         btnRegistrar.setOnClickListener(this);
         btnLogin.setOnClickListener(this);
+
     }
+
+
 
     private void registrarUsuario() {
 
@@ -160,4 +202,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void handleFacebookAccessToken(final AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(MainActivity.this, "Authentication complete.",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Intent intencion = new Intent(getApplication(), WellcomeActivity.class);
+                            intencion.putExtra(WellcomeActivity.user, user.getDisplayName());
+                            startActivity(intencion);
+                        } else{
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+
 }
